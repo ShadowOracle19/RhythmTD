b7254becae43495cfb59e27d1f00c036f1cdd073
Lucy Coates <47534892+ShadowOracle19@users.noreply.github.com>
2025-05-29 15:06:20 -0400

    update tile script to add can place tower flag

--- Assets/Scripts/Level/Tiles/Tile.cs
+++ Assets/Scripts/Level/Tiles/Tile.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-
+    public bool canPlaceTower = false;
     [SerializeField] public GameObject placedTower;
 
     private ParticleSystem spawnInstance;

ad1e5bbebac0bad9cc79411fb5c46b902580764e
Lucy Coates <47534892+ShadowOracle19@users.noreply.github.com>
2025-05-30 19:21:05 -0400

    fix showcase build

--- Assets/Scripts/Tower Defense/EncounterCreator.cs
+++ Assets/Scripts/Tower Defense/EncounterCreator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Encounter", menuName = "ScriptableObjects/EncounterCreator")]
 public class EncounterCreator : ScriptableObject
 {
+    [Header("Encounter Info")]
+    public string encounterName;
+
     [Header("Base Encounter Creator")]
     public TextAsset introDialogue;
     public CombatMaker combatEncounter;

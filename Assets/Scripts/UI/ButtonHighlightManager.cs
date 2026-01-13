a395efa768e2328c6957ac524341b2aba7d8644d
Barracle <barracle@sheridancollege.ca>
2026-01-13 14:38:53 -0500

    Animate Planet Spin Transition

--- Assets/Scripts/UI/ButtonHighlightManager.cs
+++ Assets/Scripts/UI/ButtonHighlightManager.cs
@@ -14,6 +14,10 @@ public class ButtonHighlightManager : MonoBehaviour, ISelectHandler, IDeselectHa
 
     [Header("Animation")]
 
+    [Header("Visual Changes")]
+    public bool showCharacter;
+    public GameObject characterObject;
+
     [Header("Selection Scale")]
     public Vector3 highlightScale = new Vector3(1.25f, 1.25f, 1.0f); //scale of the button when highlighted
 
@@ -29,6 +33,12 @@ public class ButtonHighlightManager : MonoBehaviour, ISelectHandler, IDeselectHa
         SoundEffectsManager.instance.PlaySound(buttonHighlightSfx, this.gameObject.transform, 1.0f);
         
         //play select animation
+
+        //change shown character (title screen only)
+        if (showCharacter) 
+        {
+            characterObject.SetActive(true);
+        }
         
         //set button scale to large
         //this.gameObject.transform.localScale = highlightScale;
@@ -40,6 +50,12 @@ public class ButtonHighlightManager : MonoBehaviour, ISelectHandler, IDeselectHa
     public void OnDeselect(BaseEventData eventData)
     {
         //play deselect animation
+
+        //change shown character (title screen only)
+        if (showCharacter) 
+        {
+            characterObject.SetActive(false);
+        }
         
         //set button scale to default
         //this.gameObject.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);

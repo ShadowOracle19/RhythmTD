fa401d2088d10c28c93de70d38a86d1501e2db2e
Barracle <barracle@sheridancollege.ca>
2026-06-14 21:39:29 -0400

    Add Pulse To Combo, Mult, & Score Text

--- Assets/Scripts/Tower Defense/Managers/ComboManager.cs
+++ Assets/Scripts/Tower Defense/Managers/ComboManager.cs
@@ -32,12 +32,20 @@ public class ComboManager : MonoBehaviour
     public int streakIncrement;
     public int score;
 
+    [Header ("Score Pulse")]
+    public GameObject cursorSprite;
+    public Vector3 textDefaultSize = new Vector3(1.0f, 1.0f, 1.0f);
+    public Vector3 textPulseSize = new Vector3(1.0f, 1.25f, 1.0f);
+
     [Header ("Score Points")]
 
     [Header ("UI Elements")]
-    public TextMeshProUGUI currentComboText;
-    public TextMeshProUGUI currentMultiplierText;
+    public TextMeshProUGUI comboText;
+    public TextMeshProUGUI multiplierText;
     public TextMeshProUGUI scoreText;
+    public Transform comboTextParent;
+    public Transform multiplierTextParent;
+    public Transform scoreTextParent;
 
     // Start is called before the first frame update
     void Start()
@@ -54,24 +62,44 @@ public class ComboManager : MonoBehaviour
     {
         currentMultiplier = Mathf.Clamp(currentMultiplier, 1, 5);
 
-        currentComboText.text = currentCombo.ToString();
-        currentMultiplierText.text = currentMultiplier.ToString();
+        comboText.text = currentCombo.ToString();
+        multiplierText.text = currentMultiplier.ToString();
         scoreText.text = score.ToString();
+
     }
 
     public void IncreaseCombo()
     {
+        comboTextParent.localScale = textPulseSize;
         currentCombo += 1; //increase combo
+        StartCoroutine(TextPulse(comboTextParent));
 
         if(currentCombo % streakIncrement == 0)
         {
+            multiplierTextParent.localScale = textPulseSize;
             currentMultiplier += 1;
+            StartCoroutine(TextPulse(multiplierTextParent));
         }
     }
 
     public void IncreaseScore(int points)
     {
+        scoreTextParent.localScale = textPulseSize;
         score += points * currentMultiplier;
+
+        StartCoroutine(TextPulse(scoreTextParent));
+    }
+
+    public IEnumerator TextPulse(Transform textParent)
+    {
+        float progress = 0.0f;
+        
+        while (textParent.localScale.x > textDefaultSize.x)
+        {
+            textParent.localScale = Vector3.Slerp(textParent.localScale, textDefaultSize, progress); //return score text to origin size
+            progress += Time.deltaTime;
+            yield return null;
+        }
     }
 
     public void ResetCombo()
